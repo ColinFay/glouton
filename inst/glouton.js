a477e86c2d49b4c4cbfe41476000309ee9c7c9ef
JohnCoene <jcoenep@gmail.com>
2019-12-29 22:32:08 +0100

    added R6Class

--- inst/glouton.js
+++ inst/glouton.js
@@ -25,5 +25,25 @@ $(document).on('shiny:connected', function(event) {
     sendCookies();
   });
 
+  // R6Class
+  function send_cookie(name){
+    var res = Cookies.get();
+    Shiny.setInputValue(name, res, {priority: "event"});
+  }
+
+  Shiny.addCustomMessageHandler('get-cookie', function(opts) {
+    var res = Cookies.get(opts.name);
+    send_cookie(opts.name);
+  });
+
+  Shiny.addCustomMessageHandler('set-cookie', function(opts) {
+    Cookies.set(opts.name, opts.value, opts.options);
+    send_cookie(opts.name);
+  });
+
+  Shiny.addCustomMessageHandler('rm-cookie', function(opts) {
+    Cookies.remove(opts.name);
+    send_cookie(opts.name);
+  });
 
 });
