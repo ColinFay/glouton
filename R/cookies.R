3cde13776e6d28615ff4df2b7f97af2994bab4e2
David Barkemeyer <david.barkemeyer@gmx.de>
2021-03-06 15:22:56 +0100

    Add support for cookie options
    - Added `cookie_options()`
    - Added argument `debug` to `add_cookie()`
    - Removed duplicate author from DESCRIPTION
    - Shortened handling of `session` argument
    - Extended example app

--- R/cookies.R
+++ R/cookies.R
@@ -41,67 +41,99 @@ use_glouton <- function(online = TRUE){
 #' `fetch_cookies` returns all the cookies, while `fetch_cookie` search for
 #' one cookie in the browser.
 #'
-#' @param session the `input` and `session` object from Shiny
-#' @param name name of the cookie to set/fetch
-#' @param value the value to set for the cookie
+#' @param session The `session` object passed to function given to
+#'   `shinyServer`. Default is [shiny::getDefaultReactiveDomain()]
+#' @param name Name of the cookie to set/fetch
+#' @param value The value to set for the cookie
+#' @param options A list of options returned from `cookie_options()`. The same
+#'   options that were passed to `add_cookie()` must be passed to
+#'   `remove_cookie()`.
+#' @param debug If `TRUE`, a message is displayed in the browser console after
+#'   adding a cookie.
+#' @param expires Define when the cookie will be removed. Value must be a
+#'   numeric, which will be interpreted as days from time of creation or
+#'   a date. If `NULL`, the cookie becomes a session cookie.
+#' @param path A string indicating the path where the cookie is visible.
+#' @param domain A string indicating a valid domain where the cookie should be
+#'   visible. The cookie will also be visible to all subdomains.
+#' @param secure Boolean indicating whether the cookie transmission requires a
+#'   secure protocol (https).
+#' @param sameSite A string allowing to control whether the browser is sending
+#'   a cookie along with cross-site requests.
 #'
 #' @export
 #' @rdname cookies
 
-fetch_cookies <- function(session = NULL){
-  if(is.null(session))
-    session <- shiny::getDefaultReactiveDomain()
+fetch_cookies <- function(session = shiny::getDefaultReactiveDomain()){
   session$sendCustomMessage("fetchcookies", TRUE)
   return(session$input[["gloutoncookies"]])
 }
 
 #' @export
 #' @rdname cookies
-fetch_cookie <- function(name, session = NULL){
-  if(is.null(session))
-    session <- shiny::getDefaultReactiveDomain()
+fetch_cookie <- function(name, session = shiny::getDefaultReactiveDomain()){
   session$sendCustomMessage("fetchcookie", TRUE)
   return(session$input[["gloutoncookies"]])
 }
 
 #' @export
 #' @rdname cookies
-add_cookie <- function(name, value, session = NULL){
-  if(is.null(session))
-    session <- shiny::getDefaultReactiveDomain()
+add_cookie <- function(name,
+                       value,
+                       options = cookie_options(),
+                       debug = FALSE,
+                       session = shiny::getDefaultReactiveDomain())
+{
   session$sendCustomMessage("addcookie", list(
-    name = name, value = value
+    name = name, value = value, options = options, debug = debug
   ))
 }
 
 #' @export
 #' @rdname cookies
-remove_cookie <- function(name, session = NULL){
-  if(is.null(session))
-    session <- shiny::getDefaultReactiveDomain()
+remove_cookie <- function(name,
+                          options = cookie_options(),
+                          session = shiny::getDefaultReactiveDomain()){
   session$sendCustomMessage("rmcookie", list(
-    name = name
+    name = name, options = options
   ))
 }
 
+#' @export
+#' @rdname cookies
+cookie_options <- function(expires = NULL,
+                           path = "/",
+                           domain = NULL,
+                           secure = FALSE,
+                           sameSite = "strict"
+) {
+  options <- list(
+    expires = expires,
+    path = path,
+    domain = domain,
+    secure = secure,
+    sameSite = sameSite
+  )
+
+  # Drop NULLs
+  options[lengths(options) != 0]
+}
+
 #' Create a Cookie
-#' 
+#'
 #' Create a cookie object.
-#' 
+#'
 #' @export
 Cookie <- R6::R6Class(
   "Cookie",
   public = list(
 #' @details Create a Cookie
-#' 
+#'
 #' @param name The name of the cookie.
 #' @param session A valid Shiny session.
-    initialize = function(name, session = NULL){
+    initialize = function(name, session = shiny::getDefaultReactiveDomain()){
       if(missing(name))
         stop("Missing `name`", call. = FALSE)
-
-      if(is.null(session))
-        session <- shiny::getDefaultReactiveDomain()
 
       private$.session <- session
       private$.name <- name
@@ -113,7 +145,7 @@ Cookie <- R6::R6Class(
       invisible(rez)
     },
 #' @details Set the value of the cookie
-#' 
+#'
 #' @param value Value of the cookie
 #' @param expires When the cookie is set to expire, integer indicating number of days.
 #' @param path Path where cookie is visible.
@@ -127,7 +159,7 @@ Cookie <- R6::R6Class(
       )
 
       private$.session$sendCustomMessage("set-cookie", list(name = private$.name, value = value, options = options))
-      
+
       invisible(self)
     },
 #' @details Remove the cookie.
